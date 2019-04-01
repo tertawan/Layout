1acd03d157bda227c1a8ea941e7c3f16dbf35dcf
Ninx <xasnaxxasnax@gmail.com>
2019-04-01 21:05:32 +0700

    Initial Commit

---
+++ Layout 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Layout 2
+//
+//  Created by Ninxi on 1/4/2562 BE.
+//  Copyright © 2562 Ninxxi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Layout 2Tests/Layout_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Layout_2Tests.swift
+//  Layout 2Tests
+//
+//  Created by Ninxi on 1/4/2562 BE.
+//  Copyright © 2562 Ninxxi. All rights reserved.
+//
+
+import XCTest
+@testable import Layout_2
+
+class Layout_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
